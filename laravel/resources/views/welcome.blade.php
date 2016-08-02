06be92811aad92b5866c4bbbb825419c20588893
heocon91095 <heocon91095@gmail.com>
2016-08-02 11:55:51 +0700

    Update them class + template

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,45 +1,115 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-                <img src="{{ URL.asset('img.png') }}">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+    <title>CSS Free Templates with jQuery Slider</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <link rel="shortcut icon" href="css/images/favicon.ico" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+    <!--[if IE 6]>
+    <script type="text/javascript" src="js/png-fix.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="js/functions.js"></script>
+</head>
+<body>
+<!-- Header -->
+<div id="header" class="shell">
+    <div id="logo"><h1><a href="#">BestSeller</a></h1><span><a href="#">free css template</a></span></div>
+    <!-- Navigation -->
+    <div id="navigation">
+        <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Promotions</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </div>
+    <!-- End Navigation -->
+    <div class="cl">&nbsp;</div>
+    <!-- Login-details -->
+    <div id="login-details">
+        <p>Welcome, <a href="#" id="user">Guest</a> .</p><p><a href="#" class="cart" ><img src="css/images/cart-icon.png" alt="" /></a>Shopping Cart (0) <a href="#" class="sum">$0.00</a></p>
+    </div>
+    <!-- End Login-details -->
+</div>
+<!-- End Header -->
+<div id="main" class="shell">
+    <!-- Sidebar -->
+    <div id="sidebar">
+        <ul class="categories">
+            <li>
+                <h4>Categories</h4>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Morbi eget</a></li>
+                    <li><a href="#">Nulla egestas</a></li>
+                    <li><a href="#">Curabitur venenatis</a></li>
+                    <li><a href="#">Ut dictum purus</a></li>
+                    <li><a href="#">Curabitur imperdiet</a></li>
+                    <li><a href="#">Aliquam elementum</a></li>
+                </ul>
+            </li>
+            <li>
+                <h4>Authors</h4>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Morbi eget</a></li>
+                    <li><a href="#">Nulla egestas</a></li>
+                    <li><a href="#">Curabitur venenatis</a></li>
+                    <li><a href="#">Ut dictum purus</a></li>
+                    <li><a href="#">Curabitur imperdiet</a></li>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Morbi eget</a></li>
+                    <li><a href="#">Nulla egestas</a></li>
+                    <li><a href="#">Curabitur venenatis</a></li>
+                    <li><a href="#">Ut dictum purus</a></li>
+                    <li><a href="#">Curabitur imperdiet</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <!-- End Sidebar -->
+    <div class="cl">&nbsp;</div>
+</div>
+<!-- End Main -->
+<!-- Footer -->
+<div id="footer" class="shell">
+    <div class="top">
+        <div class="cnt">
+            <div class="col about">
+                <h4>About BestSellers</h4>
+                <p>Nulla porttitor pretium mattis. Mauris lorem massa, ultricies non mattis bibendum, semper ut erat. Morbi vulputate placerat ligula. Fusce <br />convallis, nisl a pellentesque viverra, ipsum leo sodales sapien, vitae egestas dolor nisl eu tortor. Etiam ut elit vitae nisl tempor tincidunt. Nunc sed elementum est. Phasellus sodales viverra mauris nec dictum. Fusce a leo libero. Cras accumsan enim nec massa semper eu hendrerit nisl faucibus. Sed lectus ligula, consequat eget bibendum eu, consequat nec nisl. In sed consequat elit. Praesent nec iaculis sapien. <br />Curabitur gravida pretium tincidunt.  </p>
+            </div>
+            <div class="col store">
+                <h4>Store</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Special Offers</a></li>
+                    <li><a href="#">Log In</a></li>
+                    <li><a href="#">Account</a></li>
+                    <li><a href="#">Basket</a></li>
+                    <li><a href="#">Checkout</a></li>
+                </ul>
+            </div>
+            <div class="col" id="newsletter">
+                <h4>Newsletter</h4>
+                <p>Lorem ipsum dolor sit amet  consectetur. </p>
+                <form action="" method="post">
+                    <input type="text" class="field" value="Your Name" title="Your Name" />
+                    <input type="text" class="field" value="Email" title="Email" />
+                    <div class="form-buttons"><input type="submit" value="Submit" class="submit-btn" /></div>
+                </form>
+            </div>
+            <div class="cl">&nbsp;</div>
+            <div class="copy">
+                <p>&copy; <a href="#">BestSeller.com</a>. Design by <a href="http://css-free-templates.com/">CSS-FREE-TEMPLATES.COM</a></p>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+<!-- End Footer -->
+</body>
 </html>
